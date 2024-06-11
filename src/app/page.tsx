1bf4fba23132a546c8a162486e16b6628feebc05
Nitish <nitish.mahajanhalf8@gmail.com>
2024-06-11 14:40:46 +0530

    feat: Added storage, darkmode and other functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import QuoteCard from "@/components/QuoteCard";
 import { useEffect, useState } from "react";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 export default function Home() {
   const [data, setData] = useState("");
   const [savedQuotes, setSavedQuotes] = useState<string[]>([]);
   const [getterSetter, setGetterSetter] = useState(false);
+  const [toggleDarkMode, setToggleDarkMode] = useState(false);
   const fetchData = async () => {
     const response = await fetch(
       "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
@@ -18,46 +20,67 @@ export default function Home() {
     if (window.location !== undefined && !getterSetter) {
       setSavedQuotes(JSON.parse(localStorage.getItem("savedQuotes") || "[]"));
       setGetterSetter(true);
-      console.log("getterSetter");
     }
   }, []);
   useEffect(() => {
     if (!getterSetter) return;
     localStorage.setItem("savedQuotes", JSON.stringify(savedQuotes));
   }, [savedQuotes]);
-
   const checkQuoteInSaved = (quote: string) => {
     return savedQuotes.includes(quote);
   };
   return (
-    <main className="p-4">
-      <div className="">Current Quote: </div>
-      <QuoteCard quote={data} />
-      <div>
-        <button
-          className="p-2 text-white bg-orange-500 rounded mr-2"
-          onClick={fetchData}
-        >
-          New
-        </button>
-        <button
-          className="bg-green-600 text-white rounded  p-2"
-          onClick={() => {
-            if (checkQuoteInSaved(data)) return;
-            setSavedQuotes([...savedQuotes, data]);
-          }}
-        >
-          Save
-        </button>
-      </div>
-      {savedQuotes.length > 0 && (
-        <div>
-          <div className="">Saved Quotes: </div>
-          {savedQuotes.map((item) => (
-            <QuoteCard quote={item} />
-          ))}
+    <div className={`h-full w-full  ${toggleDarkMode ? "dark" : " "}  `}>
+      <div className={`p-4 h-[100vh] bg-white dark:bg-black dark:text-white`}>
+        <div className="flex justify-between">
+          <div>Current Quote:</div>
+          <button
+            className="border-2 border-black dark:border-white px-2 rounded"
+            onClick={() => {
+              setToggleDarkMode(!toggleDarkMode);
+            }}
+          >
+            {toggleDarkMode ? <MdLightMode color="yellow" /> : <MdDarkMode />}
+          </button>
         </div>
-      )}
-    </main>
+        <QuoteCard
+          quote={data}
+          showBin={false}
+          setSavedQuotes={setSavedQuotes}
+          savedQuotes={savedQuotes}
+        />
+        <div>
+          <button
+            className="p-2 text-white bg-orange-500 rounded mr-2"
+            onClick={fetchData}
+          >
+            New
+          </button>
+          <button
+            className="bg-green-600 text-white rounded  p-2"
+            onClick={() => {
+              if (checkQuoteInSaved(data)) return;
+              setSavedQuotes([...savedQuotes, data]);
+            }}
+          >
+            Save
+          </button>
+        </div>
+        {savedQuotes.length > 0 && (
+          <div>
+            <div className="mt-10">Saved Quotes: </div>
+            {savedQuotes.map((item) => (
+              <QuoteCard
+                key={item}
+                quote={item}
+                showBin={true}
+                setSavedQuotes={setSavedQuotes}
+                savedQuotes={savedQuotes}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { IoTrashBin } from "react-icons/io5";
 type QuoteCardProps = {
   quote: string;
+  showBin: boolean;
+  setSavedQuotes: (quote: string[]) => void;
+  savedQuotes: string[];
 };
-const QuoteCard = ({ quote }: QuoteCardProps) => {
+const QuoteCard = ({
+  quote,
+  showBin,
+  setSavedQuotes,
+  savedQuotes,
+}: QuoteCardProps) => {
+  const deleteQuote = (quote: string) => {
+    const newSavedQuotes = savedQuotes.filter((item) => item !== quote);
+    setSavedQuotes(newSavedQuotes);
+  };
   return (
-    <div className="border-2 border-black rounded-lg p-4 w-auto my-2">
+    <div className="border-2 border-black dark:border-white rounded-lg flex justify-between items-center p-4 w-auto my-2">
       {quote}
+
+      {showBin && (
+        <button
+          onClick={() => {
+            deleteQuote(quote);
+          }}
+        >
+          <IoTrashBin />
+        </button>
+      )}
     </div>
   );
 };
